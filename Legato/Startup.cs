f9ed3d244f943ce43394d267ca5b76ab338d59ea
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-20 07:36:37 +0200

    Add Swagger generator to the services collection

--- Legato/Startup.cs
+++ Legato/Startup.cs
@@ -19,6 +19,11 @@ namespace Legato
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Legato Web API", Version = "v1"});
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
