d36812df46763430445d715bc85e58ad17d7ceec
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-21 19:44:59 +0200

    Add properties to the RegisterUser class

--- Legato/Models/RegisterUser.cs
+++ Legato/Models/RegisterUser.cs
@@ -1,10 +1,42 @@
-﻿namespace Legato.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Legato.Models
 {
     /// <summary>
     ///     RegisterUser model which contains the necessary properties to register a user
     /// </summary>
     public class RegisterUser
     {
-        
+        /// <summary>
+        ///     Fist name of the user to register
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        ///     Last name of the user to register
+        /// </summary>
+        [Required]
+        public string LastName { get; set; }
+
+        /// <summary>
+        ///     Birthdate of the user to register
+        /// </summary>
+        [Required]
+        public DateTime Birthdate { get; set; }
+
+        /// <summary>
+        ///     Home country of the user to register
+        /// </summary>
+        [Required]
+        public string Country { get; set; }
+
+        /// <summary>
+        ///     Favorite music genres of the user to register
+        /// </summary>
+        [Required]
+        public List<string> Genres { get; set; }
     }
 }
