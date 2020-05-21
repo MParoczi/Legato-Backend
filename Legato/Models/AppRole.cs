4296ad21e0db87c7b8ebb88d8700c5d14d7f36f8
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-21 19:13:52 +0200

    Extend AppRole class with IdentityRole class

--- Legato/Models/AppRole.cs
+++ Legato/Models/AppRole.cs
@@ -1,10 +1,12 @@
-﻿namespace Legato.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Legato.Models
 {
     /// <summary>
     ///     Represents an application role that can be requested by (and granted to) a client application, or that can be used
     ///     to assign an application to users or groups in a specified role.
     /// </summary>
-    public class AppRole
+    public class AppRole : IdentityRole<int>
     {
     }
 }
