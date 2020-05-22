580f97c7c3c3866bdb66eb4f81e48732026c376c
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-22 13:01:15 +0200

    Add ConfigureSqlConnection method

--- Legato/Extensions/ServiceExtensions.cs
+++ Legato/Extensions/ServiceExtensions.cs
@@ -1,3 +1,9 @@
+﻿using System;
+using Legato.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
 namespace Legato.Extensions
 {
     /// <summary>
@@ -5,5 +11,16 @@ namespace Legato.Extensions
     /// </summary>
     public static class ServiceExtensions
     {
+        /// <summary>
+        ///     The ConfigureSqlConnection method creates a connection between the application and the PostgreSQL server
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureSqlConnection(this IServiceCollection services)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("LEGATO_CONNECTION_STRING");
+
+            services.AddDbContext<IdentityAppContext>(cfg =>
+                cfg.UseNpgsql(connectionString ?? throw new NpgsqlException()));
+        }
     }
 }
