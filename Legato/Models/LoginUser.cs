3d7b9f8876e030e0bae2f4eb5ed5de75426a7fe8
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-22 12:25:37 +0200

    Remove data annotations from LoginUser class

--- Legato/Models/LoginUser.cs
+++ Legato/Models/LoginUser.cs
@@ -1,8 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.AspNetCore.Identity;
-
-namespace Legato.Models
+﻿namespace Legato.Models
 {
     /// <summary>
     ///     LoginUser model which contains the necessary properties to login a user
@@ -12,15 +8,11 @@ namespace Legato.Models
         /// <summary>
         ///     Email address of the user to login
         /// </summary>
-        [Required]
-        [ProtectedPersonalData]
         public string Email { get; set; }
 
         /// <summary>
         ///     Password of the user to login
         /// </summary>
-        [NotMapped]
-        [ProtectedPersonalData]
         public string Password { get; set; }
     }
 }
