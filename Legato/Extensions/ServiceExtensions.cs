e29293eb0410aca131ea35ee3e1956e34314c01f
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-22 12:51:28 +0200

    Add XML comment to ServiceExtensions class declaration

--- Legato/Extensions/ServiceExtensions.cs
+++ Legato/Extensions/ServiceExtensions.cs
@@ -1,7 +1,9 @@
 ﻿namespace Legato.Extensions
 {
+    /// <summary>
+    ///     The ServiceExtensions class contains extension methods to configure services in the Startup class
+    /// </summary>
     public class ServiceExtensions
     {
-        
     }
 }
