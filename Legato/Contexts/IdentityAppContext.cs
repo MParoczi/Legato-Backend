b6ed0acdfba738fbef52791eca6a2b4102ac56f2
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-25 10:22:10 +0200

    Add XML comment to IdentityAppContext class declaration

--- Legato/Contexts/IdentityAppContext.cs
+++ Legato/Contexts/IdentityAppContext.cs
@@ -1,6 +1,10 @@
-﻿namespace Legato.Contexts
+namespace Legato.Contexts
 {
-    public class IdentityAppContext
+    /// <summary>
+    ///     A DbContext instance represents a combination of the Unit Of Work and Repository patterns such that it can be used
+    ///     to query from a database and group together changes that will then be written back to the store as a unit.
+    /// </summary>
+    public class IdentityAppContext : IdentityDbContext<AppUser, AppRole, int>
     {
         
     }
