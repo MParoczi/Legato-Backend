b1d4c898c7f5107ab97133f3cfbc1279d8ac6601
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-25 20:12:55 +0200

    Clean imports

--- Legato/Controllers/AccountController.cs
+++ Legato/Controllers/AccountController.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Legato.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Legato.Controllers
