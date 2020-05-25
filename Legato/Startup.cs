bc95d33efab2004a74ce50b23e2c753b7fb4aad2
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-25 10:08:43 +0200

    Add SQL connection to services

--- Legato/Startup.cs
+++ Legato/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using Legato.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -75,6 +76,8 @@ namespace Legato
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
+            
+            services.ConfigureSqlConnection();
         }
 
         /// <summary>
