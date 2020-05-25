8dff507bbe9a97fbd2dea233b68e0caf749429ee
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-25 20:12:19 +0200

    Add XML comment for the AccountController class declaration

--- Legato/Controllers/AccountController.cs
+++ Legato/Controllers/AccountController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Legato.Controllers
 {
+    /// <summary>
+    ///     Controller class that handles HTTP Requests for the registration, login and logout of the users
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
