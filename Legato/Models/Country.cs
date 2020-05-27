2d4b9b280e9737d82386a2bb3dcc1b86c74e22dc
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-27 11:01:56 +0200

    Add Country model

---
+++ Legato/Models/Country.cs
@@ -0,0 +1,22 @@
+﻿namespace Legato.Models
+{
+    /// <summary>
+    ///     POCO for country
+    /// </summary>
+    public class Country
+    {
+        /// <summary>
+        ///     Constructor for Country class
+        /// </summary>
+        /// <param name="name">Name of the country</param>
+        public Country(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        ///     Name property of the country
+        /// </summary>
+        public string Name { get; }
+    }
+}
