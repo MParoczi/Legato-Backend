2455c9cfb862ed0a9338650512521f5d119e5f8c
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-28 09:34:57 +0200

    Add SendAsync method

--- EmailService/EmailSender.cs
+++ EmailService/EmailSender.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using MailKit.Net.Smtp;
 using MimeKit;
 using MimeKit.Text;
@@ -45,6 +46,24 @@ namespace EmailService
             finally
             {
                 client.Disconnect(true);
+                client.Dispose();
+            }
+        }
+
+        private async Task SendAsync(MimeMessage mailMessage)
+        {
+            using var client = new SmtpClient();
+            try
+            {
+                await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
+
+                await client.SendAsync(mailMessage);
+            }
+            finally
+            {
+                await client.DisconnectAsync(true);
                 client.Dispose();
             }
         }
