d586f41f861b8ef5a1d6d78bbe6edfa7a53c4790
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-01 09:04:39 +0200

    Add XML comment to the UserDto class

--- Legato/Models/UserDto.cs
+++ Legato/Models/UserDto.cs
@@ -1,8 +1,9 @@
 ﻿namespace Legato.Models
 {
-    
+    /// <summary>
+    ///     Simple POCO object to transfer user information to the frontend
+    /// </summary>
     public class UserDto
     {
-        
     }
 }
