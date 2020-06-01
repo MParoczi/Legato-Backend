8dc55111ee8ea772493b6e7c4a086eaa28d2fd81
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-01 16:42:10 +0200

    Add JWT credentials

--- Legato/Models/JwtCredentials.cs
+++ Legato/Models/JwtCredentials.cs
@@ -1,10 +1,25 @@
-﻿namespace Legato.Models
+﻿using System;
+
+namespace Legato.Models
 {
     /// <summary>
     ///     Contains constants that needs to configure the JWT token provider
     /// </summary>
     public class JwtCredentials
     {
-        
+        /// <summary>
+        ///     Issuer of the JWT token
+        /// </summary>
+        public static readonly string Issuer = "Legato";
+
+        /// <summary>
+        ///     Audience of the JWT token
+        /// </summary>
+        public static readonly string Audience = "ApiUser";
+
+        /// <summary>
+        ///     Secret key for the JWT token
+        /// </summary>
+        public static readonly string Key = Environment.GetEnvironmentVariable("LEGATO_SECRET_KEY");
     }
 }
