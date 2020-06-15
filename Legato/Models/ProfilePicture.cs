874bc0b2643009fd9ef8ec839933afab9c9e99e6
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-15 16:38:17 +0200

    Add ProfilePicture model class

---
+++ Legato/Models/ProfilePicture.cs
@@ -0,0 +1,13 @@
+﻿namespace Legato.Models
+{
+    /// <summary>
+    ///     Model for the profile picture changes
+    /// </summary>
+    public class ProfilePicture
+    {
+        /// <summary>
+        ///     Cloudinary URL for the profile picture of the user
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
