729fec936156d34d951c32c1c08ba273799ad72d
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-17 14:17:30 +0200

    Implement Post class

--- Legato/Models/PostModel/Post.cs
+++ Legato/Models/PostModel/Post.cs
@@ -1,7 +1,35 @@
-﻿namespace Legato.Models.PostModel
+﻿using Legato.Models.UserModels;
+
+namespace Legato.Models.PostModel
 {
+    /// <summary>
+    ///     POCO class to represent a post created by a user
+    /// </summary>
     public class Post
     {
-        
+        /// <summary>
+        ///     Unique identification for a single post
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///     Title of the post
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        ///     Content of the post
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        ///     Id of the user that created the post
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        ///     Navigation property of the user
+        /// </summary>
+        public AppUser User { get; set; }
     }
 }
