db80fc375602bbe52f4fa6b71308cc58feb71e21
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-17 15:01:23 +0200

    Implement PostEntityTypeConfiguration configuration class

--- Legato/Contexts/Configuration/PostEntityTypeConfiguration.cs
+++ Legato/Contexts/Configuration/PostEntityTypeConfiguration.cs
@@ -1,7 +1,24 @@
-﻿namespace Legato.Contexts.Configuration
+﻿using Legato.Models.PostModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Legato.Contexts.Configuration
 {
-    public class PostEntityTypeConfiguration
+    /// <summary>
+    ///     Configuration for Post model on database creating
+    /// </summary>
+    public class PostEntityTypeConfiguration : IEntityTypeConfiguration<Post>
     {
-        
+        /// <summary>
+        ///     Configure post model on database creating
+        /// </summary>
+        /// <param name="builder">Provides a simple API for configuring an IMutableEntityType.</param>
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder
+                .HasOne(p => p.User)
+                .WithMany(u => u.Posts)
+                .HasForeignKey(p => p.UserId);
+        }
     }
 }
