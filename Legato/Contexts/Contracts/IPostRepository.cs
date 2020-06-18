86e4c3f74337f8b43a0d77d620d4bd72cf140759
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-18 08:54:04 +0200

    Add IPostRepository interface

---
+++ Legato/Contexts/Contracts/IPostRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Legato.Contexts.Contracts
+{
+    /// <summary>
+    ///     Contains database operations for the Post model
+    /// </summary>
+    public interface IPostRepository
+    {
+    }
+}
